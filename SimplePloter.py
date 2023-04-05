e9c018abcb1fa0474994edb25224f8a84b210059
Godbito <garbuinitzky@outlook.com>
2023-04-05 16:53:47 -0400

    Imported matplotlib library and created the first plotter with some small styling

--- SimplePloter.py
+++ SimplePloter.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 
 squares = [1,4,9,16,25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+#set char title and label axes.
+ax.set_title("Square numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+#set tickness of the label
+ax.tick_params(labelsize=14)
 
 plt.show()
